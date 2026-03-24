233bbe91f2fc77667ae05a59c47a5ba53b1b0630
vladimir-rusakov <virt.p.q@yandex.ru>
2026-03-24 23:21:04 +0300

    добавил документацию к классу ApiService

--- src/components/ApiService.ts
+++ src/components/ApiService.ts
@@ -6,15 +6,40 @@ import {
   IApiProductList,
 } from "../types";
 
+/**
+ * Класс для получения данных о товарах с сервера и отправки на сервер данных о покупателе и выбранных товарах.
+ */
 export class ApiService {
+  /**
+   * Объект, соответсвующий интерфейсу `IApi`, для взаимодействия с сервером
+   * @private
+   */
   private api: IApi;
+
+  /**
+   * Эндпоинт для получения описания товаров от сервера 
+   * @private
+   */
   private productUri: string = "/product/";
+
+  /**
+   * Эндпоинт для отправки данных о покупателе на сервер
+   * @private
+   */
   private postOrderUri: string = "/order/";
 
+  /**
+   * Создает экземпляр класса для отправки запросов на сервер
+   * @param api - объект, соответсвующий `IApi`, для взаимодействия с сервером
+   */
   constructor(api: IApi) {
     this.api = api;
   }
 
+  /**
+   * Получение данных о товарах от сервера
+   * @returns количество товаров в поле `total`, массив с описанием товаров в поле `items`
+   */
   async getProductList(): Promise<IApiProductList> {
     let products: IApiProductList | void;
 
@@ -23,6 +48,12 @@ export class ApiService {
     return products || [];
   }
 
+  /**
+   * Отправка данных пользователя на сервер
+   * @param data - поля `IBuyer`, поле `total` со стоимостью корзины и поле `items` с массивом идентификаторов товаров из корзины
+   * @returns объект с суммой заказа в поле `total` и идентификатором заказа в поле `id` в случае успешного запроса. 
+   * иначе - объект с описанием ошибки в поле `error`
+   */
   async postBuyerData(
     data: IApiBuyerPostData,
   ): Promise<IApiPostResponseSuccess | IApiPostResponseError> {
