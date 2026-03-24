16d0acc150bb12a460baf1448db990217b95eb8f
vladimir-rusakov <virt.p.q@yandex.ru>
2026-03-24 22:22:02 +0300

    тест моделей данных в связке с апи сервера

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { Api } from './components/base/Api';
 import './scss/styles.scss';
 import { IApiBuyerPostData } from './types';
 import { API_URL } from './utils/constants';
+import { Basket } from './components/models/Basket.ts';
+import { ProductCatalog } from './components/models/ProductCatalog.ts';
+import { Buyer } from './components/models/Buyer.ts';
+import './scss/styles.scss';
+
 
 
 const api = new Api(API_URL);
@@ -15,7 +20,7 @@ products.items.forEach(item => {
   console.log(item.id, item.image);
 })
 
-const buyer: IApiBuyerPostData = {
+const buyerData: IApiBuyerPostData = {
     "payment": "cash",
     "email": "test@test.ru",
     "phone": "+71234567890",
@@ -28,8 +33,109 @@ const buyer: IApiBuyerPostData = {
 }
 
 try {
-  const postResp = await apiService.postBuyerData(buyer);
+  const postResp = await apiService.postBuyerData(buyerData);
   console.log(postResp)
 } catch (error) {
   console.log(error)
 }
+
+
+// код ниже - тест модеоей данных из коммита d7dfdff77836b3d4116f665830beb4fa5181b288 ветки dev
+
+
+const productsModel = new ProductCatalog(products.items);
+
+productsModel.items.forEach(item => {
+  console.log(`Товар из каталога: ${item.id}`);
+})
+
+console.log(`selectedItem до выбора: ${productsModel.selectedItem}`);
+
+productsModel.selectedItem = products.items[0];
+
+console.log(`selectedItem после выбора: ${productsModel.selectedItem.id}`);
+
+const basket = new Basket();
+
+console.log(`пустая корзина: ${basket.items}`);
+
+basket.add(products.items[1]);
+basket.add(products.items[2]);
+
+console.log(`непустая корзина: ${basket.items}`);
+basket.items.forEach(item => {
+  console.log(`Товар из корзины: ${item.id}`);
+})
+
+basket.delete(products.items[3].id);
+basket.delete(products.items[2].id);
+
+console.log(`непустая корзина: ${basket.items}`);
+basket.items.forEach(item => {
+  console.log(`Товар из корзины: ${item.id}`);
+})
+
+basket.add(products.items[0]);
+basket.add(products.items[2]);
+basket.add(products.items[3]);
+basket.add(products.items[4]);
+basket.add(products.items[44]);
+
+console.log(`Товар из корзины: ${products.items[44]}`);
+
+console.log(`цена корзины: ${basket.getTotalCost()}`);
+
+basket.delete(products.items[3].id);
+
+console.log(`наличие товара (существующего): ${basket.has(products.items[0].id)}`);
+console.log(`наличие товара (существующего): ${basket.has(products.items[1].id)}`);
+console.log(`наличие товара (существующего): ${basket.has(products.items[2].id)}`);
+console.log(`наличие товара (несуществующего): ${basket.has(products.items[3].id)}`);
+console.log(`наличие товара (несуществующего): ${basket.has('')}`);
+console.log(`наличие товара (несуществующего): ${basket.has('abc')}`);
+
+
+const buyer = new Buyer();
+
+console.log(`покупатель без данных: ${Object.entries(buyer.getData())}`);
+
+buyer.clearData();
+
+console.log(`покупатель без данных после очистки данных: ${Object.entries(buyer.getData())}`);
+
+console.log(`валидация покупателя без данных: ${Object.entries(buyer.validateData())}`);
+
+buyer.saveData({'payment': 'cash'});
+
+console.log(`покупатель с данными payment: ${Object.entries(buyer.getData())}`);
+
+console.log(`валидация покупателя с данными payment: ${Object.entries(buyer.validateData())}`);
+
+buyer.saveData({'address': '5 ave, NY'});
+
+console.log(`покупатель с данными address: ${Object.entries(buyer.getData())}`);
+
+console.log(`валидация покупателя с данными address: ${Object.entries(buyer.validateData())}`);
+
+buyer.saveData({'email': 'a@a.a'});
+
+console.log(`покупатель с данными email: ${Object.entries(buyer.getData())}`);
+
+console.log(`валидация покупателя с данными email: ${Object.entries(buyer.validateData())}`);
+
+buyer.saveData({'phone': '123'});
+
+console.log(`покупатель с данными phone: ${Object.entries(buyer.getData())}`);
+
+console.log(`валидация покупателя с данными phone: ${Object.entries(buyer.validateData())}`);
+
+buyer.clearData();
+
+console.log(`покупатель после очистки данных: ${Object.entries(buyer.getData())}`);
+
+buyer.saveData({'payment': 'card'});
+buyer.saveData({'address': '5 ave, NY'});
+buyer.saveData({'email': 'a@a.a'});
+buyer.saveData({'phone': '123'});
+
+console.log(buyer.payment, buyer.address , buyer.email , buyer.phone );
